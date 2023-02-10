771078e53806e9b576ed799b0841e1b7e78fe801
angelicacvo <angiemarin0707@gmail.com>
2023-02-10 01:12:28 -0500

    tamagotchi terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tamagotchi';
+  hunger = 70;
+  sleepiness = 70;
+  life = 100;
+
+    tamagotchi = {
+    hunger: 0,
+    sleep: 0,
+  }
+
+  eat() {
+    this.hunger += 5;
+  }
+
+  sleep() {
+    this.sleepiness += 5;
+  }
+
+  start() {
+    this.hunger = 100;
+    this.sleepiness = 100;
+    this.life = 100;
+  }
+  
+  lifebar() {
+    this.hunger -= 0.05;
+    this.sleepiness -= 0.05;
+    if (this.hunger <= 50 && this.sleepiness <= 50) {
+      this.life -= 1;
+    }
+    if (this.hunger >= 50 && this.sleepiness >= 50) {
+      this.life += 0.4;
+    }
+  }
 }
